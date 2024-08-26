5425aa9cff1056c46c3c33cd8a3fa3dc860d25e6
LunaYi101 <joonha138@gmail.com>
2024-08-26 12:57:53 +0900

    모델 다시 추가

--- sentiment.py
+++ sentiment.py
@@ -3,16 +3,15 @@ import json
 import ast
 import random
 import string
+from transformers import pipeline
+import torch
 
-# from transformers import pipeline
-# import torch
-
-# if torch.cuda.is_available():
-#     device = torch.device("cuda")
-# elif torch.backends.mps.is_available():
-#     device = torch.device("mps")
-# else:
-#     device = torch.device("cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
 
 # 1. pos neg
 # classifier = pipeline("text-classification", model="matthewburke/korean_sentiment")
@@ -21,20 +20,20 @@ import string
 # classifier = pipeline("text-classification", model="smilegate-ai/kor_unsmile")
 
 # 3. 감정
-# classifier = pipeline("text-classification", model="hun3359/klue-bert-base-sentiment", device=device)
+classifier = pipeline("text-classification", model="hun3359/klue-bert-base-sentiment", device=device)
 
-# # custom_tweet = ast.literal_eval(sys.argv[1])
-# custom_tweet = '테스트'
-# preds = classifier(custom_tweet, top_k=None)
+# custom_tweet = ast.literal_eval(sys.argv[1])
+custom_tweet = '테스트'
+preds = classifier(custom_tweet, top_k=None)
 
-# sorted_preds = sorted(preds, key=lambda x: x['score'], reverse=True)
+sorted_preds = sorted(preds, key=lambda x: x['score'], reverse=True)
 
 # Function to generate a random string of length 4
-def generate_random_label():
-    return ''.join(random.choices(string.ascii_lowercase, k=4))
+# def generate_random_label():
+#     return ''.join(random.choices(string.ascii_lowercase, k=4))
 
-# Create a list of 60 dictionaries
-sorted_preds = [{'label': generate_random_label(), 'score': random.uniform(0, 1)} for _ in range(60)]
+# # Create a list of 60 dictionaries
+# sorted_preds = [{'label': generate_random_label(), 'score': random.uniform(0, 1)} for _ in range(60)]
 
 for item in sorted_preds:
     item['score'] = round(item['score'], 5)
